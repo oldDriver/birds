eb5d2e283a8dc83835227b76d6a7ac227d3212a0
oleksii.emelyanov <OEmelyanov@spherasolution.com>
2024-07-01 22:24:58 +0200

    Redirect after login

---
+++ src/Security/Authentication/AuthenticationSuccessHandler.php
@@ -0,0 +1,25 @@
+<?php
+// src/Security/Authentication/AuthenticationSuccessHandler.php
+namespace App\Security\Authentication;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class AuthenticationSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    public function __construct(private UrlGeneratorInterface $router)
+    {}
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token): RedirectResponse
+    {
+        $user = $token->getUser();
+        $roles = $user->getRoles();
+        if (in_array('ROLE_ADMIN', $roles)) {
+            return new RedirectResponse($this->router->generate('admin'));
+        }
+        return new RedirectResponse($this->router->generate('app_dashboard'));
+    }
+}
